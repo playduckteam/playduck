1d89e7008c5c054afb3004b8ddc70fff91494568
suyeong <suyeong@61.73.125.160>
2021-06-07 14:34:10 +0900

    2021-06-07 Commit 	-mypage 작업 	- header에 mypage로 이동하는 텝 추가

--- playduck/src/main/java/com/duck/playduck/mypage/model/dao/MypageDAO.java
+++ playduck/src/main/java/com/duck/playduck/mypage/model/dao/MypageDAO.java
@@ -1,5 +1,10 @@
 package com.duck.playduck.mypage.model.dao;
 
+import com.duck.playduck.member.model.vo.Member;
+
 public interface MypageDAO {
+
+
+
 
 }
--- playduck/src/main/java/com/duck/playduck/mypage/model/service/MypageServiceImpl.java
+++ playduck/src/main/java/com/duck/playduck/mypage/model/service/MypageServiceImpl.java
@@ -2,13 +2,21 @@ package com.duck.playduck.mypage.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import com.duck.playduck.member.model.vo.Member;
 import com.duck.playduck.mypage.model.dao.MypageDAO;
 
 @Service
 public class MypageServiceImpl implements MypageService {
 
 	@Autowired
-	MypageDAO mypageDao;
+	MypageDAO mypageDAO;
+
+
+
+	
+
+	
 	
 }
